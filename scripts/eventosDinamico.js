ebee1b9abc91e6886fc022383a376afc72e0f304
AX414 <68911216+AX414@users.noreply.github.com>
2020-10-07 15:05:47 -0300

    Update eventosDinamico.js

--- scripts/eventosDinamico.js
+++ scripts/eventosDinamico.js
@@ -1,12 +1,13 @@
 function gerenciarAltoContraste() {
     console.log("funcao gerenciar alto contraste");
 
-    var corAtual, elementoBody;
+    var corAtual, elementoH1, elementoBody;
     var apresentacao1, apresentacao2, apresentacao3, apresentacao4, apresentacao5, apresentacao6, apresentacao7;
 
     // buscando elemento body da pagina
     // elemento na primeira posicao da lista (body so tem um)
-    elementoBody = document.getElementsByTagName("body")[0]; // 1° posicao
+    elementoBody = document.getElementsByTagName("body")[0];
+    elementoH1 = document.getElementsByTagName("h1");
     apresentacao1 = document.getElementById("apresentacao1");
     apresentacao2 = document.getElementById("apresentacao2");
     apresentacao3 = document.getElementById("apresentacao3");
@@ -14,8 +15,8 @@ function gerenciarAltoContraste() {
     apresentacao5 = document.getElementById("apresentacao5");
     apresentacao6 = document.getElementById("apresentacao6");
     apresentacao7 = document.getElementById("apresentacao7");
+    corAtual = elementoBody.style.backgroundColor;
 
-    corAtual = elementoBody.style.backgroundColor;     // obtem cor atual    
     console.log("cor atual: " + corAtual);
 
     if (corAtual == "black") { // esta com fundo preto, ou seja, alto contraste
@@ -33,6 +34,7 @@ function gerenciarAltoContraste() {
         elementoBody.style.backgroundImage = "none";
         elementoBody.style.backgroundColor = "black";    // cor de fundo preta
         elementoBody.style.color = "white";
+
         apresentacao1.style.backgroundColor = "black";
         apresentacao2.style.backgroundColor = "black";
         apresentacao3.style.backgroundColor = "black";
@@ -40,9 +42,37 @@ function gerenciarAltoContraste() {
         apresentacao5.style.backgroundColor = "black";
         apresentacao6.style.backgroundColor = "black";
         apresentacao7.style.backgroundColor = "black";
-        document.getElementById("acao3").src = "imagens/ação/Herobrine.png";
+
     }
 
+
+}
+
+function ativarExtra() {
+    console.log("funcao ativar extra");
+
+    var body = document.getElementsByTagName("body")[0];
+
+    var apresentacao1 = document.getElementById("apresentacao1");
+    var apresentacao2 = document.getElementById("apresentacao2");
+    var apresentacao3 = document.getElementById("apresentacao3");
+    var apresentacao4 = document.getElementById("apresentacao4");
+    var apresentacao5 = document.getElementById("apresentacao5");
+    var apresentacao6 = document.getElementById("apresentacao6");
+    var apresentacao7 = document.getElementById("apresentacao7");
+
+    body.style.backgroundImage = "url(../imagens/ender.jpg)";
+    body.style.backgroundColor = "#282a36";
+    body.style.color = "#ff79c6";
+    
+
+    apresentacao1.style.backgroundColor = "#282a36";
+    apresentacao2.style.backgroundColor = "#282a36";
+    apresentacao3.style.backgroundColor = "#282a36";
+    apresentacao4.style.backgroundColor = "#282a36";
+    apresentacao5.style.backgroundColor = "#282a36";
+    apresentacao6.style.backgroundColor = "#282a36";
+    apresentacao7.style.backgroundColor = "#282a36";
 
 }
 
@@ -58,7 +88,7 @@ function voltarNormal() {
     var apresentacao6 = document.getElementById("apresentacao6");
     var apresentacao7 = document.getElementById("apresentacao7");
 
-    body.style.backgroundImage = "url(imagens/background.jpg)";
+    body.style.backgroundImage = "url(../imagens/background.jpg)";
     body.style.color = "black";
 
     apresentacao1.style.backgroundColor = "#f7f7f7";
@@ -82,6 +112,7 @@ window.onload = function () {
 
     // vincular, associar (bind, trigger, listener)
     document.getElementById("opAltoContraste").addEventListener("click", gerenciarAltoContraste);
+    document.getElementById("opExtra").addEventListener("click", ativarExtra);
     document.getElementById("opNormal").addEventListener("click", voltarNormal);
 
 
